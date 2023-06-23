d58714df07f87e443dcaaa8b03d2085ae11d428a
Powerlax <raovishruth@gmail.com>
2023-06-23 15:41:41 -0700

    Added Nessesary Steps

---
+++ Main.py
@@ -0,0 +1,23 @@
+def get_random_quote():
+    #TODO: Find a random quote
+    pass
+
+def sentiment_analysis(quote):
+    #TODO: Run Sentiment Analysis on quote
+    pass
+
+def get_picture(results):
+    #TODO: Find a relevent picture based on results of Sentiment Analysis
+    pass
+
+def find_text_color(picture):
+    #TODO: Find the constasting color to the picture to use as text color
+    pass
+
+def combine(quote, picture):
+    #TODO: Put the text on the picture
+    pass
+
+def export_into_file(new_picture):
+    #TODO: Export into a file called quote.png
+    pass
