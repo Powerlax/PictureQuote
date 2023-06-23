e7f82d168a7d5cd2edc37742579ae5ff6dc7ee3c
Powerlax <raovishruth@gmail.com>
2023-06-23 16:21:27 -0700

    Added main and get_random_quote function

--- Main.py
+++ Main.py
@@ -1,9 +1,16 @@
+import requests
+import json
+
 def get_random_quote():
-    #TODO: Find a random quote
-    pass
+    #We are using the API from https://www.github.com/lukePeavey/quotable
+    r = requests.get("https://api.quotable.io/random")
+    if (r.status_code == requests.codes.ok):
+        return json.loads(r.text)
 
 def sentiment_analysis(quote):
     #TODO: Run Sentiment Analysis on quote
+    #We need to find emotion of the quote and the message
+    
     pass
 
 def get_picture(results):
